5b4f2732522195cecd35dd547d37a2274bf44609
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-25 21:10:34 +0530

    Refactored:Create Table of columns id, name and salary

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -1,5 +1,12 @@
-public class EmployeePayroll {
-	public static void main (String[] args) {
-        	System.out.println("Welcome to the Employee Payroll problem in Database"):
-	}
-}
+CREATE DATABASE employee_payroll;
+show databases;
+USE payroll_service;
+SELECT DATABASE();
+
+CREATE TABLE employee_payroll (
+	id					INT unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name				VARCHAR(255) NOT NULL,
+    salary				DOUBLE NOT NULL,
+    start				DATE NOT NULL
+);
+DESCRIBE employee_payroll;
