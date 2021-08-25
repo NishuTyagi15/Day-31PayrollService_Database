a6443c645c77d05c90190e6c7b4e1df3c3563824
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-25 21:24:06 +0530

    Refactored:Added gender column and update row to reflect the correct employee gender

--- Employee_Payroll.sql
+++ Employee_Payroll.sql
@@ -21,3 +21,11 @@ SELECT * FROM employee_payroll;
 SELECT salary FROM employee_payroll WHERE name = 'Krati';
 SELECT * FROM employee_payroll 
 	WHERE start BETWEEN CAST('2021-07-07' AS DATE) AND DATE(NOW());
+    
+ALTER TABLE employee_payroll ADD gender CHAR(6) AFTER name;
+UPDATE employee_payroll set gender = 'Female' WHERE name = 'Nishu' OR name = 'Krati';
+UPDATE employee_payroll set gender = 'Male' WHERE name = 'Shivam';
+DESCRIBE employee_payroll;
+SELECT * FROM employee_payroll;
+UPDATE employee_payroll set salary = 900000.0 WHERE name = 'Nishu';
+SELECT * FROM employee_payroll;
